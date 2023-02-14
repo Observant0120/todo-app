312f4a23d8da82e38552f390139563674620a528
baek <vjw21@naver.com>
2023-02-14 15:47:13 +0900

    dev 브랜치에 모든 파일 첫 커밋

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          레포지토리 적용하기
         </p>
         <a
           className="App-link"
