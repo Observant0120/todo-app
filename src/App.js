ee10b1befcd8871d53007e80d288f616b9de60ad
baek <vjw21@naver.com>
2023-02-14 16:31:00 +0900

    dev => main 테스트 1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          레포지토리 적용하기 (연습하기)
+          레포지토리 적용하기
         </p>
         <a
           className="App-link"
